675c5dd8ea4e02918cb841d35fb502bff4968545
siahreds <issiahstephenson9@gmail.com>
2026-08-16 05:05:38 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,82 @@
+const SUFFIELD = {
+
+  school: {
+    name: "Suffield Academy",
+    system: "Suffield SIMs",
+    academicYear: "2026/27"
+  },
+
+  forms: [
+    "7A",
+    "7B",
+    "8A",
+    "8B",
+    "9A",
+    "9B",
+    "10A",
+    "10B",
+    "11A",
+    "11B",
+    "12A"
+  ],
+
+  departments: {
+    core: [
+      "Mathematics",
+      "English",
+      "Science"
+    ],
+
+    pe: [
+      "Physical Education",
+      "Sports Science"
+    ],
+
+    humanities: [
+      "History",
+      "Geography",
+      "Religious Education",
+      "Business Studies"
+    ],
+
+    socialSciences: [
+      "Sociology",
+      "Economics",
+      "Psychology",
+      "Health & Social Care",
+      "Child Development"
+    ],
+
+    creativeArts: [
+      "Art & Design",
+      "Drama",
+      "Music"
+    ],
+
+    technology: [
+      "Food Technology",
+      "Design Technology",
+      "Computer Science",
+      "Fashion Design"
+    ],
+
+    mfl: [
+      "French",
+      "Spanish",
+      "German"
+    ]
+  },
+
+  students: [],
+
+  staff: [],
+
+  timetables: [],
+
+  attendance: [],
+
+  behaviour: [],
+
+  assessments: []
+
+};
